197842772e0966390b7d8fae822dde3ce45f06f1
sajjad ghorbani <sajjadr2001@gmail.com>
2022-08-03 20:12:41 +0430

    Add project files.

---
+++ NQueens/Server/Controllers/NVazirController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NQueens.Server.Services;
+
+namespace NQueens.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NVazirController : ControllerBase
+    {
+        private NVazirService _nVazirService;
+
+        [HttpPost]
+        public IActionResult NVazir([FromBody] int n)
+        {
+
+            _nVazirService = new NVazirService(n);
+            _nVazirService.Queens(0);
+            var answers = _nVazirService.answers;
+
+            int i = 1;
+            List<List<int>> answersJsonObject = new List<List<int>>();
+            while (i * n <= answers.Count)
+            {
+                var reshte = answers.Skip((i - 1) * n).Take(n).ToList();
+                answersJsonObject.Add(reshte);
+                i++;
+            }
+            return Ok(answersJsonObject);
+        }
+
+    }
+}
---
+++ NQueens/Server/Services/NVazirService.cs
@@ -0,0 +1,66 @@
+﻿namespace NQueens.Server.Services
+{
+    public class NVazirService
+    {
+        //  public int[] col;
+        public Array col;
+        public List<int> answers = new List<int>();
+        static int N;
+        public NVazirService(int n)
+        {
+            col = Array.CreateInstance(typeof(int), new int[1] { n }, new int[1] { 1 });
+            //  col = new int[n+1];
+            N = n;
+
+
+        }
+        public void Queens(int i)
+        {
+            if (Promising(i))
+            {
+
+                if (i == N)
+                {
+
+                    foreach (var item in col)
+                    {
+                        // if(item!=0)
+                        // answers.Add(item);
+                        answers.Add((int)item);
+                    }
+                    return;
+                }
+
+                else
+                {
+                    i++;
+                    for (int j = 1; j <= N; j++)
+                    {
+                        // col[i]=j;
+                        col.SetValue(j, i);
+                        Queens(i);
+                    }
+
+                }
+            }
+        }
+        public bool Promising(int i)
+        {
+            int k;
+            bool check;
+            k = 1;
+            check = true;
+            while (k < i && check)
+            {
+                int col_i = Convert.ToInt32(col.GetValue(i));
+                int col_k = Convert.ToInt32(col.GetValue(k));
+
+                if (col_i == col_k || Math.Abs(col_i - col_k) == i - k)
+                    check = false;
+                k++;
+            }
+            return check;
+        }
+
+    }
+}
